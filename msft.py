f733002de382a7723c4a3f4b7e35ea1303d265f0
ritwikthakar <80002638+ritwikthakar@users.noreply.github.com>
2021-07-16 00:59:27 -0400

    Add files via upload

---
+++ msft.py
@@ -0,0 +1,38 @@
+import dash
+import dash_html_components as html
+import pandas as pd
+import dash_bootstrap_components as dbc
+import numpy as np
+import FundamentalAnalysis as fa
+from config import k
+ticker = 'AAPL'
+
+
+Income_Statement = fa.income_statement(ticker, k, period="annual")
+Balance_Sheet = fa.balance_sheet_statement(ticker, k, period="annual")
+Cash_Flow_Statement = fa.cash_flow_statement(ticker, k, period="annual")
+
+def generate_table(dataframe, max_rows=45):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(Microsoft=[
+    html.H4(Microsoft='Microsoft Balance Sheet'),
+    generate_table(Balance_Sheet)
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
